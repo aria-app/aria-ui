a55563fb40ca04cc0e1bfe7dc8d3b0e9d1e610d6
Nick Johnson <nickjohnson.dev@gmail.com>
2021-06-22 12:51:20 -0500

    feat: Add Tab and Tabs components

---
+++ stories/Components/Tab.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+
+import { Tab, TabProps } from '../../src';
+
+export default {
+  title: 'Components/Tab',
+  component: Tab,
+  argTypes: {
+    onSelect: { action: 'onSelect' },
+  },
+} as Meta;
+
+export const Default: Story<TabProps> = args => <Tab {...args} />;
+
+Default.args = {
+  disabled: false,
+  isSelected: false,
+  label: 'Tab Label',
+  value: 'tab value',
+};
+
+export const Selected: Story<TabProps> = args => <Tab {...args} />;
+
+Selected.args = {
+  ...Default.args,
+  isSelected: true,
+};
+
+export const Disabled: Story<TabProps> = args => <Tab {...args} />;
+
+Disabled.args = {
+  ...Default.args,
+  disabled: true,
+};
