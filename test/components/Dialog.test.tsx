08c0fb5cbf4be7f919c8293374a2b5ce918fc361
Nick Johnson <nickjohnson.dev@gmail.com>
2021-06-29 07:38:22 -0500

    Fill out further tests for Dialog

--- test/components/Dialog.test.tsx
+++ test/components/Dialog.test.tsx
@@ -5,17 +5,37 @@ import React from 'react';
 import { render, screen } from '../../src/testUtils';
 import * as stories from '../../stories/Components/Dialog.stories';
 
-const { Default, Closed } = composeStories(stories);
+const { Closed, Default } = composeStories(stories);
 
 describe('Dialog', () => {
-  test('should register overlay clicks by default', () => {
+  test('should handle clicking confirm', () => {
+    const handleConfirm = jest.fn();
+
+    render(<Default onConfirm={handleConfirm} />);
+
+    userEvent.click(screen.getByText('Confirm'));
+
+    expect(handleConfirm).toHaveBeenCalledWith(expect.anything());
+  });
+
+  test('should handle clicking cancel', () => {
+    const handleCancel = jest.fn();
+
+    render(<Default onCancel={handleCancel} />);
+
+    userEvent.click(screen.getByText('Cancel'));
+
+    expect(handleCancel).toHaveBeenCalledWith(expect.anything());
+  });
+
+  test('should handle overlay clicks by default', () => {
     const handleOverlayClick = jest.fn();
 
     render(<Default onOverlayClick={handleOverlayClick} />);
 
     userEvent.click(screen.getByTestId('aria-ui-dialog-overlay'));
 
-    expect(handleOverlayClick).toHaveBeenCalledTimes(1);
+    expect(handleOverlayClick).toHaveBeenCalledWith(expect.anything());
   });
 
   test('should not render clickable overlay when closed', () => {
