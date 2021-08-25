1e40b082582b1fd85564e377278e21a849fa5e29
Nick Johnson <nickjohnson.dev@gmail.com>
2021-08-25 07:21:13 -0500

    Get definitions emitting from rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require('./package.json');
 
@@ -19,5 +20,10 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [commonjs(), nodeResolve(), typescript()],
+  plugins: [
+    peerDepsExternal(),
+    nodeResolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+  ],
 };
